dac3e6d13b7dacd1175dc06de7f7fffd075ad93f
Pierre Roy <lrp.roy@gmail.com>
2022-05-22 00:07:09 +0200

    Project cleaning

--- Assets/ImportantPrototype/Scripts/Pickable/PickableItem.cs
+++ Assets/ImportantPrototype/Scripts/Pickable/PickableItem.cs
@@ -4,9 +4,6 @@ namespace ImportantPrototype.Pickable
 {
     public class PickableItem : MonoBehaviour
     {
-        [SerializeField]
-        private string _name;
-        
         public void OnTriggerEnter2D(Collider2D other)
         {
             Destroy(gameObject);
